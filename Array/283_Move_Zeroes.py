334ae6cec9f56f35cb205d89334ad7d83de71259
Vaibhav_sinha <2k23.csdsc2312114@gmail.com>
2026-02-13 17:47:08 +0530

    Add moveZeroes method to shift zeros in array
    Implement moveZeroes method to shift zeros in the list.

---
+++ Array/283_Move_Zeroes.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution(object):
+    def moveZeroes(self, nums):
+        i=0
+        count=0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        return nums
