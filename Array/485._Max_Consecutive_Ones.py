b91afffc256bd42a3fbbe5d00484716453892223
Vaibhav_sinha <2k23.csdsc2312114@gmail.com>
2026-02-15 22:54:12 +0530

    Add solution for finding max consecutive ones

---
+++ Array/485._Max_Consecutive_Ones.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        count=0
+        maxi=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                count+=1
+                maxi=max(maxi,count)
+            else:
+                count=0
+        return maxi
